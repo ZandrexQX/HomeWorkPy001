3ffbab260fc6fdfc419909cc8069619cabbe8473
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-03-15 06:43:46 +0300

    Task 6

--- HomeWork001.py
+++ HomeWork001.py
@@ -1,22 +1,38 @@
-print('Task 2 \n ---------')
+# print('Task 2 \n ---------')
 
-num = int(input("Введите число: "))
-result = 0
+# num = int(input("Введите число: "))
+# result = 0
 
-while num > 0:
-    result += num%10
-    num = int(num/10)
+# while num > 0:
+#     result += num%10
+#     num = int(num/10)
 
-print(int(result))
-input()
+# print(int(result))
+# input()
 
-# ---------------------------------
+# # ---------------------------------
 
-print('Task 4 \n ---------')
+# print('Task 4 \n ---------')
 
-num = int(input("Введите число: "))
-print(f"Петя: {int(num/6)}  Катя: {int(num/6*4)}  Сережа: {int(num/6)}")
+# num = int(input("Введите число журавликов: "))
+# print(f"Петя: {int(num/6)}  Катя: {int(num/6*4)}  Сережа: {int(num/6)}")
 
-# ---------------------------------
+# # ---------------------------------
 
 print('Task 6 \n ---------')
+
+num = int(input("Введите номер билета: "))
+
+lenNum = len(str(num))
+middle = float(lenNum/2)
+a = 0
+b = 0
+
+while lenNum > 0:
+    if lenNum >= middle+1: a += int(num%10)
+    elif lenNum <= middle: b += int(num%10)
+    num /= 10
+    lenNum -= 1
+    
+if a == b: print("Yes")
+else: print("No")
