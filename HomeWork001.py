8e4999dbe63d616bb9e0e5d180465f997364d164
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-03-15 07:16:06 +0300

    Task 8

--- HomeWork001.py
+++ HomeWork001.py
@@ -1,23 +1,24 @@
-# print('Task 2 \n ---------')
+print('Task 2 \n ---------')
 
-# num = int(input("Введите число: "))
-# result = 0
+num = int(input("Введите число: "))
+result = 0
 
-# while num > 0:
-#     result += num%10
-#     num = int(num/10)
+while num > 0:
+    result += num%10
+    num = int(num/10)
 
-# print(int(result))
-# input()
+print(int(result))
+input()
 
-# # ---------------------------------
+# ---------------------------------
 
-# print('Task 4 \n ---------')
+print('Task 4 \n ---------')
 
-# num = int(input("Введите число журавликов: "))
-# print(f"Петя: {int(num/6)}  Катя: {int(num/6*4)}  Сережа: {int(num/6)}")
+num = int(input("Введите число журавликов: "))
+print(f"Петя: {int(num/6)}  Катя: {int(num/6*4)}  Сережа: {int(num/6)}")
 
-# # ---------------------------------
+input()
+# ---------------------------------
 
 print('Task 6 \n ---------')
 
@@ -36,3 +37,16 @@ while lenNum > 0:
     
 if a == b: print("Yes")
 else: print("No")
+
+input()
+# ---------------------------------
+
+print('Task 8 \n ---------')
+n = int(input("Введите n: "))
+m = int(input("Введите m: "))
+k = int(input("Введите k: "))
+
+if ((k%n == 0) or (k%m == 0)) and (k < m*n): print("yes")
+else: print("No")
+
+input()
