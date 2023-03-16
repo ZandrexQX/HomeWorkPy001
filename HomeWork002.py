081ca2ad3a01114683720792d0fb7af26e2c8ab0
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-03-16 12:23:21 +0300

    Task Add 1

--- HomeWork002.py
+++ HomeWork002.py
@@ -10,9 +10,8 @@ from random import randint as r
 #     if i == 0: count_0 += 1
 #     else: count_1 += 1
 
-# if (len(list_1) - count_1) > (len(list_1) - count_0):
-#     result = len(list_1) - count_0
-# else: result = len(list_1) - count_1
+# if count_0 > count_1: result = count_1
+# else: result = count_0
 
 # print(f"Монеты: {list_1}")
 # print(f"Надо перевернуть монет: {result}")
@@ -42,20 +41,43 @@ from random import randint as r
 
 # # ---------------------------------
 
-print('Task 14 \n ---------')
+# print('Task 14 \n ---------')
 
-n = int(input("Введите число N: "))
+# n = int(input("Введите число N: "))
 
-count = 0
-result = 1
+# count = 0
+# result = 1
 
-while (result < n) and result != n:
-     result *= 2
-     count += 1
-     if result <= n: 
-        print(f"2 в степени {count} равна: {result}")   # Развернутый ответ
-        # print(result)                                 # Просто вывод результата
+# while result <= n:
+#     print(f"2 в степени {count} равна: {result}")   # Развернутый ответ
+#     # print(result)                                 # Просто вывод результата
+#     result *= 2
+#     count += 1 
+    
+# input()
 
-input()
+# # ---------------------------------
 
-# ---------------------------------
+print('Task Add 1 \n ---------')
+
+list_1 = [r(1,1000) for i in range(r(2,5))]
+print(*list_1)
+str_1 =""
+def action(list_1):
+    global str_1
+    if len(list_1) == 0: 
+        print(str_1)
+        return
+    max = max_i = a = 0
+    for i in range(len(list_1)):
+        x = list_1[i]
+        while x/10 > 1: x = x/10
+        if x > max: 
+            max = x
+            max_i = i
+    print(list_1[max_i])
+    str_1 += str(list_1[max_i])
+    list_1.remove(list_1[max_i])
+    action(list_1)
+
+action(list_1)
