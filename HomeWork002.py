91314b79921767616dd50552d4b13b077cdb34eb
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-03-16 06:18:58 +0300

    Task 12

--- HomeWork002.py
+++ HomeWork002.py
@@ -1,42 +1,42 @@
 from random import randint as r
 
-print('Task 10 \n ---------')
-n = int(input("Введите число монет: "))
+# print('Task 10 \n ---------')
+# n = int(input("Введите число монет: "))
 
-list_1 = [r(0,1) for i in range(n)]
-count_0 = count_1 = result = 0
+# list_1 = [r(0,1) for i in range(n)]
+# count_0 = count_1 = result = 0
 
-for i in list_1:
-    if i == 0: count_0 += 1
-    else: count_1 += 1
+# for i in list_1:
+#     if i == 0: count_0 += 1
+#     else: count_1 += 1
 
-if (len(list_1) - count_1) > (len(list_1) - count_0):
-    result = len(list_1) - count_0
-else: result = len(list_1) - count_1
+# if (len(list_1) - count_1) > (len(list_1) - count_0):
+#     result = len(list_1) - count_0
+# else: result = len(list_1) - count_1
 
-print(f"Монеты: {list_1}")
-print(f"Надо перевернуть монет: {result}")
+# print(f"Монеты: {list_1}")
+# print(f"Надо перевернуть монет: {result}")
 
-input()
+# input()
 
-# ---------------------------------
+# # ---------------------------------
 
 print('Task 12 \n ---------')
-n = int(input("Введите число монет: "))
 
-list_1 = [r(0,1) for i in range(n)]
-count_0 = count_1 = result = 0
+a = r(0,1000)
+b = r(0,1000)
 
-for i in list_1:
-    if i == 0: count_0 += 1
-    else: count_1 += 1
+sum = a + b
+mult = a*b
+c = 0
 
-if (len(list_1) - count_1) > (len(list_1) - count_0):
-    result = len(list_1) - count_0
-else: result = len(list_1) - count_1
+while c*(sum-c) != mult:
+    c += 1
 
-print(f"Монеты: {list_1}")
-print(f"Надо перевернуть монет: {result}")
+d = sum - c
+
+print(f"Петя загадал: {a} и {b}")
+print(f"Катя отгадала: {c} и {d}")
 
 input()
 
