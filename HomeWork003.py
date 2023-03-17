5340593c752276c3b4085c4c5bae710905683dc2
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-03-18 00:43:31 +0300

    Task add 1

--- HomeWork003.py
+++ HomeWork003.py
@@ -46,31 +46,51 @@ from random import randint as r
 
 # # ---------------------------------
 
-print('Task 20 \n ---------')
+# print('Task 20 \n ---------')
 
-n = input("Введите слово:").upper()
+# n = input("Введите слово:").upper()
 
-letters_1 = {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T',
-             'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т' }
+# letters_1 = {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T',
+#              'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т' }
 
-letters_2 = {'D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'}
-letters_3 = {'B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'}
-letters_4 = {'F', 'H', 'V', 'W', 'Y', 'Й', 'Ы'}
-letters_5 = {'K', 'Ж', 'З', 'Х', 'Ц', 'Ч'}
-letters_8 = {'J', 'X', 'Ш', 'Э', 'Ю'}
-letters_10 = {'Q', 'Z', 'Ф', 'Щ', 'Ъ'}
+# letters_2 = {'D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'}
+# letters_3 = {'B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'}
+# letters_4 = {'F', 'H', 'V', 'W', 'Y', 'Й', 'Ы'}
+# letters_5 = {'K', 'Ж', 'З', 'Х', 'Ц', 'Ч'}
+# letters_8 = {'J', 'X', 'Ш', 'Э', 'Ю'}
+# letters_10 = {'Q', 'Z', 'Ф', 'Щ', 'Ъ'}
 
-count = 0
-for i in n:
-    if i in letters_1: count += 1
-    elif i in letters_2: count += 2
-    elif i in letters_3: count += 3
-    elif i in letters_4: count += 4
-    elif i in letters_5: count += 5
-    elif i in letters_8: count += 8
-    elif i in letters_10: count += 10
+# count = 0
+# for i in n:
+#     if i in letters_1: count += 1
+#     elif i in letters_2: count += 2
+#     elif i in letters_3: count += 3
+#     elif i in letters_4: count += 4
+#     elif i in letters_5: count += 5
+#     elif i in letters_8: count += 8
+#     elif i in letters_10: count += 10
 
-print(count)
+# print(count)
+
+# input()
+
+# # ---------------------------------
+
+print('Task Add 1 \n ---------')
+
+string_1 = [i for i in input("Введите первую фразу:").
+            strip().lower() if i.isalpha()]
+string_2 = [i for i in input("Введите вторую фразу:").
+            strip().lower() if i.isalpha()]
+
+for i in string_1:
+    if i in string_2:
+        string_2.remove(i)
+    else: 
+        print("NO")
+        break
+    
+if len(string_2) == 0: print("YES")
 
 input()
 
