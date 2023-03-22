3e7c374f1191666350503605250b418168ab4259
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-03-22 06:38:11 +0300

    Task 24

--- HomeWork004.py
+++ HomeWork004.py
@@ -17,3 +17,18 @@ print(*list_result)
 input()
 
 # --------------------------------------------------------
+
+print('Task 24 \n ---------')
+
+n = int(input("Введите количество элементов N (от 3):"))
+while n < 3: n = int(input("Введите количество элементов N (от 3):"))
+
+list_num_1 = [r(1,50) for i in range(1,n)]
+
+max_count = list_num_1[0] + list_num_1[1] + list_num_1[2]
+for i in range(len(list_num_1)-1):
+    temp = list_num_1[i-1] + list_num_1[i] + list_num_1[i+1]
+    if temp > max_count: max_count = temp
+
+print(list_num_1)
+print(max_count)
