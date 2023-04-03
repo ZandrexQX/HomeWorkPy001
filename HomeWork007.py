f53a88a6b62d177e9f8d1819dbe4c28d0c580a91
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-04-03 07:51:02 +0300

    edit

--- HomeWork007.py
+++ HomeWork007.py
@@ -6,7 +6,7 @@ list_words = input("Введите фразу: ").split()
 def count_chars(word):
     count = 0
     for j in word:
-        if j in chars:
+        if j.lower() in chars:
             count += 1
     return count
 
@@ -15,3 +15,24 @@ if len(set(map(lambda x: count_chars(x), list_words))) == 1:
 else: print("Пам парам")
 
 # ---------------------------------
+
+print('Task 36 \n ---------')
+
+n = int(input("Введите количество: "))
+
+list_0 = [[0]*n]*n
+
+for i in list_0:
+    for j in i:
+        print(j, end = "\t")
+    print()
+    
+print(list_0)
+
+
+def print_operation_table(f, num_rows, num_columns):
+    pass
+    
+
+
+print_operation_table(lambda x, y: x * y, num_rows=6, num_columns=6)
