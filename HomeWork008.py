98006867b96c2c42fd3dd4750a0ee724cf315901
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-04-05 21:21:44 +0300

    edit

--- HomeWork008.py
+++ HomeWork008.py
@@ -2,14 +2,36 @@ import shelve as s
 
 keys = ("name",
         "family",
-        "family_2",
         "phone")
 
-with s.open("phonebook") as key:
-    key[keys[1]] = "Aleksey"
-    # states["Paris"] = "France"
-    # states["Berlin"] = "Germany"
-    # states["Madrid"] = "Spain"
+contacts = list()
+reading_list = list()
+
+def add_contact(l):
+    name = input("Введите имя: ")
+    family = input("Введите фамилию: ")
+    phone = input("Введите телефон: ")
     
-with s.open("phonebook") as key:
-    print(key[keys[1]])
+    contact = {keys[0]:name, keys[1]: family, keys[2]: phone}
+    l.append(contact)
+
+add_contact(contacts)
+
+# contacts.append(contact_1)
+# contacts.append(contact_2)
+
+print(contacts)
+
+with s.open("phonebook") as data:
+    data["0"] = contacts
+        
+with s.open("phonebook") as data:
+    reading_list = data["0"]
+    
+print(reading_list)
+
+for i in reading_list:
+    print("Name: " + i[keys[0]])
+    print("Family: " + i[keys[1]])
+    print("Phone: " + i[keys[2]])
+    print()
